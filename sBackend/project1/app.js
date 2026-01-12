dbf93ab2fdbcb8ef453a2891425ddc7f5bf02b61
yashwantkumar7256 <yashwantkumar7256@gmail.com>
2026-01-12 15:21:16 +0530

    login page

---
+++ sBackend/project1/app.js
@@ -0,0 +1,68 @@
+const express =require('express');
+const app=express();
+const path =require('path')
+const userModel=require('./models/UserData');
+const bcrypt=require('bcrypt')
+const cookie=require('cookie-parser');
+const jwt=require('jsonwebtoken')
+
+app.set('view engine', 'ejs')
+app.use(express.json())
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));
+app.use(cookie())
+
+
+
+
+
+
+app.get('/', (req,res)=>{
+    res.render("index");
+});
+app.post('/create', (req,res)=>{
+   const {name ,email,password,age}=req.body;
+  bcrypt.genSalt(10,(err,salt)=>{
+    bcrypt.hash(password,salt , async(err,hash)=>{
+
+        const created= await userModel.create({
+        name,
+        email,
+        password:hash,
+        age,
+     });
+      let token =jwt.sign({email},'shhh')
+      res.cookie('token',token)
+  res.send(created);
+
+    })
+
+  })
+})
+app.get('/login',(req,res)=>{
+   
+    res.render("login")
+});
+app.post('/login' , async (req,res)=>{
+    const {email,password}=req.body;
+    let user= await userModel.findOne({email});
+    if(!user) return res.send("something went wrong");
+    bcrypt.compare(password,user.password, (err,result)=>{
+        if(result){
+            let token=jwt.sign({email:user.email} ,'shhh')
+            res.cookie('token',token)
+
+            res.send('you loged in succusfully');
+        }else{
+            res.send("something gone wrong")
+        }
+    })
+
+  
+
+  
+});
+
+app.listen(3000,()=>{
+    console.log("working")
+})
---
+++ sBackend/project1/models/UserData.js
@@ -0,0 +1,13 @@
+
+const mongoose=require('mongoose');
+
+mongoose.connect(`mongodb://127.0.0.1:27017/UserData`);
+
+const userSchema=mongoose.Schema({
+    name:String,
+    email:String,
+    password:String,
+    age:Number,
+
+});
+module.exports =mongoose.model('user',userSchema);
